5cdf8e80c9a717c41929d2ce8fc118408fc43abd
Laure <laure.laforgue@gmail.com>
2018-07-10 16:00:13 +0200

    add before and after

--- src/test/java/test2_TP5.java
+++ src/test/java/test2_TP5.java
@@ -1,16 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class test2_TP5 {
 
     WebDriver driver;
+
+    @Before
+    public void init(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver. get("http://www.amazon.fr");
+    }
+
     @Test
     public void test1(){
-        driver = new ChromeDriver();
-        driver. get("http://google.com");
+
+    }
+
+    @After
+    public void tearDown(){
         driver.quit();
     }
 }
