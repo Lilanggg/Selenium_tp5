754131ee8a777a673e31016fa70421087fa5817a
Laure <laure.laforgue@gmail.com>
2018-07-10 15:15:07 +0200

    creation test2

--- src/test/java/test2_TP5.java
+++ src/test/java/test2_TP5.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.WeakHashMap;
+
 public class test2_TP5 {
+
+    WebDriver driver;
+    @Test
+    public void test1(){
+        driver = new ChromeDriver();
+        driver. get("http://google.com");
+        driver.quit();
+    }
 }
